68e95d018f02c0cb9accc9f057556afb10dbdb80
dievenegas <diegovenegasanabalon@gmail.com>
2023-11-03 20:26:15 -0300

    Cambio en test bmh

--- Algoritmo1/test.cpp
+++ Algoritmo1/test.cpp
@@ -5,11 +5,10 @@
 #include <chrono>
 #include <vector>
 
-using namespace std;
 using namespace std::chrono;
 
 
-string* LecturaArchivo(const string &directorio) {
+string* LecturaArchivo(const string &directorio, int tamStr) {
     ifstream archivo(directorio);
     if (!archivo.is_open()) {
         throw runtime_error("Error al abrir el archivo.");
@@ -17,12 +16,12 @@ string* LecturaArchivo(const string &directorio) {
     stringstream buffer;
     buffer << archivo.rdbuf();
     archivo.close();
-    return new string(buffer.str());
+    return new string(buffer.str().substr(0, tamStr));
 }
 
 
 struct tiempos {
-    unsigned int PreComputo, ContPatron, SumTotal, size;
+    unsigned int PreComputo, ContPatron, SumTotal;
 };
 
 tiempos Test(string* &texto, string patron) {
@@ -40,50 +39,53 @@ tiempos Test(string* &texto, string patron) {
     resultados.ContPatron =  duration_cast<microseconds>(end - start).count();
 
     resultados.SumTotal = resultados.ContPatron + resultados.PreComputo;
-    resultados.size = sizeof(bmh.PTBad);
 
     delete[] bmh.PTBad;
     return resultados;
 } 
 
 
-int main() {
+int main(int argc, char* argv[]) {
 
+    if(argc != 2) {
+        cout << "Uso: " << argv[0] << " n" << endl;
+        return 1;
+    }
+    int tamStr = atoi(argv[1]);
+    
     string* texto;
     try {
-        texto = LecturaArchivo("../dna.50MB.txt"); // Alternativamente "../../dna.100MB.txt"
+        texto = LecturaArchivo("../dna.50MB.txt", tamStr); // Alternativamente "../../dna.100MB.txt"
     } catch (const exception& e) {
         cerr << e.what() << endl;
         return 1; 
     }
 
-    unsigned int PreCompTotal = 0, ContPatrTotal = 0,  sumaTotal = 0, PreCompCuadrados = 0, ContPatrCuadrados = 0, TotalCuadrados = 0;
+    unsigned int PreCompTotal = 0, ContPatrTotal = 0,  sumaTotal = 0;
 
     vector<tiempos> resultados;
     for(int i = 0 ; i < 30 ; ++i){
         resultados.push_back(Test(texto, "AACCT"));
-
-        cout << resultados.at(i).PreComputo << ";" << resultados.at(i).ContPatron << ";" << resultados.at(i).SumTotal << ";" << resultados.at(i).size << "\n";
-
         PreCompTotal += resultados.at(i).PreComputo;
         ContPatrTotal += resultados.at(i).ContPatron;
         sumaTotal += resultados.at(i).SumTotal;
-        PreCompCuadrados += resultados.at(i).PreComputo * resultados.at(i).PreComputo;
-        ContPatrCuadrados += resultados.at(i).ContPatron * resultados.at(i).ContPatron;
-        TotalCuadrados += resultados.at(i).SumTotal * resultados.at(i).SumTotal;
     }
 
-    double promPre = static_cast<double>(PreCompTotal) / resultados.size();
-    double promCont = static_cast<double>(ContPatrTotal) / resultados.size();
-    double promTot = static_cast<double>(sumaTotal) / resultados.size();
-    double varPre = static_cast<double>(PreCompCuadrados) / resultados.size() - promPre * promPre;
-    double varCont = static_cast<double>(ContPatrCuadrados) / resultados.size() - promCont * promCont;
-    double varTot = static_cast<double>(TotalCuadrados) / resultados.size() - promTot * promTot;
+    double promPre = static_cast<double>(PreCompTotal) / 30;
+    double promCont = static_cast<double>(ContPatrTotal) / 30;
+    double promTot = static_cast<double>(sumaTotal) / 30;
+
+    double varPre = 0, varCont = 0, varTot = 0;
+    for(int i = 0 ; i < 30 ; ++i){
+        varPre += (static_cast<double>(resultados.at(i).PreComputo) - promPre) * (static_cast<double>(resultados.at(i).PreComputo) - promPre);
+        varCont += (static_cast<double>(resultados.at(i).ContPatron) - promCont) * (static_cast<double>(resultados.at(i).ContPatron) - promCont);
+        varTot += (static_cast<double>(resultados.at(i).SumTotal) - sumaTotal) * (static_cast<double>(resultados.at(i).SumTotal) - sumaTotal);
+    }
+    varPre = varPre/30;
+    varCont = varCont/30;
+    varTot = varTot/30;
     
-    cout << "Promedio;Promedio;Promedio" << "\n";
-    cout << promPre << ";" << promCont << ";" << promTot << "\n";
-    cout << "Varianza;Varianza;Varianza" << "\n";
-    cout << varPre << ";" << varCont << ";" << varTot << "\n";
+    cout << promPre << ";" << promCont << ";" << promTot << ";" << varPre << ";" << varCont << ";" << varTot << endl;
 
     delete texto;
     return 0;
